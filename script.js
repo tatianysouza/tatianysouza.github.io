c5d6172b2634bdc5c14c8d2b3991a3a2a06f4531
tatiany <tatianysouza2018@gmail.com>
2023-11-19 16:46:12 -0300

    adicionando apresentação

---
+++ script.js
@@ -0,0 +1,22 @@
+const texto = document.getElementById('texto');
+const palavra = 'HELLOO';
+const binario = palavra.split('').map(char => char.charCodeAt(0).toString(2)).join(' ');
+
+let i = 0;
+let decodificado = '';
+
+const intervalo = setInterval(() => {
+    if (binario[i] === ' ') {
+        decodificado += String.fromCharCode(parseInt(binario.slice(i - 7, i), 2));
+        i++; 
+    }
+
+    texto.textContent = decodificado + binario.slice(i);
+
+    i++;
+
+    if (i > binario.length) {
+        clearInterval(intervalo);
+        texto.textContent = decodificado;
+    }
+}, 75);
